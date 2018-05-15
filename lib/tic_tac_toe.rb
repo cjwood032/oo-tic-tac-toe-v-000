ac3eec4fed7342a09067be141fb2acc8a2a98872
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-15 08:32:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,7 @@
 class TicTacToe
   def initialize(board)
-    board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
-WIN_COMBINATIONS = [
+    @board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -11,9 +10,8 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [0,4,8],
   [6,4,2]
-]
-
-def play(board)
+    ]
+  def play(board)
   while !over?(board)
     turn(board)
   end
@@ -22,45 +20,45 @@ def play(board)
   elsif draw?(board)
     puts "Cat's Game!"
   end
-end
+  end
 
-def display_board(board)
+  def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+  end
 
-def valid_move?(board, index)
+  def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
-end
+  end
 
-def won?(board)
+  def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
   end
-end
+  end
 
-def full?(board)
+  def full?(board)
   board.all?{|token| token == "X" || token == "O"}
-end
+  end
 
-def draw?(board)
+  def draw?(board)
   !won?(board) && full?(board)
-end
+  end
 
-def over?(board)
+  def over?(board)
   won?(board) || draw?(board)
-end
+  end
 
-def input_to_index(user_input)
+  def input_to_index(user_input)
   user_input.to_i - 1
-end
+  end
 
-def turn(board)
+  def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
@@ -70,26 +68,27 @@ def turn(board)
   else
     turn(board)
   end
-end
+  end
 
-def position_taken?(board, index)
+  def position_taken?(board, index)
   board[index]== "X" || board[index] == "O"
-end
+  end
 
-def current_player(board)
+  def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
-end
+  end
 
-def turn_count(board)
+  def turn_count(board)
   board.count{|token| token == "X" || token == "O"}
-end
+  end
 
-def move(board, index, player)
+  def move(board, index, player)
   board[index] = player
-end
+  end
 
-def winner(board)
+  def winner(board)
   if winning_combo = won?(board)
     board[winning_combo.first]
+  end
   end
 end
